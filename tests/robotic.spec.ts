622d9eef4a25e4604c251ba344ad60e73ea58fb3
Your Name <you@example.com>
2023-08-18 16:11:30 +0100

    init repo

---
+++ tests/robotic.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://aeu-wssas0022/WHDS.RoboticPicking.Web/ngApp/#/');
+  await page.getByRole('link', { name: 'Pallets extracted from Zone' }).click();
+  await page.getByRole('button', { name: 'Search' }).click();
+  await page.getByRole('cell', { name: '10000044' }).click();
+});
